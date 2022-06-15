f3a2e41ff330eba05807b77351de1614ade99c99
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2022-06-15 21:25:50 +0100

    flush stdout

--- src/spectral_solvers/spectral_solvers.py
+++ src/spectral_solvers/spectral_solvers.py
@@ -8,7 +8,7 @@ try:
     from slepc4py import SLEPc
     can_use_slepc = True
 except ImportError:
-    print('slepc4py not installed, will use scipy')
+    print('Can not import slepc/petc not installed, will use scipy', flush=True)
     can_use_slepc = False
 
 from .basis import ChebychevBasis, BoundaryCondition, HermiteBasis, LaguerreBasis

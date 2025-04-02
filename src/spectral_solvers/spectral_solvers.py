115337e6e1a850b13d954a7c03ca7a410e1dfbc5
SijmeJan <14568532+SijmeJan@users.noreply.github.com>
2025-04-02 10:26:29 +0200

    add 2d spectral solver

--- src/spectral_solvers/spectral_solvers.py
+++ src/spectral_solvers/spectral_solvers.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.linalg import eigvals, eig
 import scipy.sparse as sparse
+from scipy.sparse.linalg import LinearOperator, gmres, cgs
+
 #import matplotlib.pyplot as plt
 
 try:
@@ -85,7 +87,7 @@ def safe_eval_evec(eval_low, evec_low, eval_hi, evec_hi,
       eval_low_and_indx[np.where(inverse_drift > drift_threshold)]
 
     eval_low = eval_low_and_indx[:, 0]
-    indx = eval_low_and_indx[:, 1].real.astype(np.int)
+    indx = eval_low_and_indx[:, 1].real.astype(np.int64)
 
     evec = []
     for i in indx:
@@ -117,6 +119,7 @@ class SpectralSolver():
         self.N = -1
 
     def set_resolution(self, N, L=1, n_eq=1):
+        #print('Number of equations:', n_eq)
         # Coordinate mapping and basis functions
         if self.basis_kind == 'Hermite':
             self.mapping = CoordinateMap(L=L)
@@ -142,12 +145,22 @@ class SpectralSolver():
 
         # Collocation points in t and z
         t = self.basis.collocation_points()
+        N = len(t)
         self.z = self.mapping.x(t)
 
         # Construct A, A' and A''
         self.construct_a()
 
         # Block-diagonal matrix with A's on the diagonal
+
+        # he standard BSR representation where the block column indices for
+        # row i are stored in indices[indptr[i]:indptr[i+1]] and their
+        # corresponding block values are stored in data[ indptr[i]: indptr[i+1] ].
+
+        # indices = [0, 1, 2, 3, 4]
+        # indptr = [0, 1, 2, 3, 4, 5]
+        # block column index for row i: indices[i] = i
+
         indptr = np.asarray(range(0, n_eq + 1))
         indices = np.asarray(range(0, n_eq))
         data = []
@@ -193,7 +206,20 @@ class SpectralSolver():
             return np.matmul(np.diag(f), self.dA)
         return np.matmul(np.diag(f), self.A)
 
+    def fast_evaluate(self, sol, n_eq=1):
+        t = self.mapping.t(self.z)
+
+        L = len(self.basis.collocation_points())
+        sol = sol.reshape((n_eq, L))
+
+        for i in range(0, n_eq):
+            sol[i,:] = np.matmul(self.A, sol[i,:])
+
+        return t, sol
+
     def evaluate(self, z, sol, n_eq=1, k=0):
+        #print('Number of equations:', n_eq)
+
         t = self.mapping.t(z)
 
         L = len(self.basis.collocation_points())
@@ -223,6 +249,7 @@ class SpectralSolver():
         return self.transform(z, u, n_eq=n_eq)
 
     def transform(self, z, u, n_eq=1):
+        #print('Number of equations:', n_eq)
         return u
 
 class BoundaryValueProblemSolver(SpectralSolver):
@@ -240,14 +267,19 @@ class BoundaryValueProblemSolver(SpectralSolver):
         return self.z
 
     def matrixM(self):
-        return self.matrixP + self.matrixQ + self.matrixR
+        return self.matrixP() + self.matrixQ() + self.matrixR()
 
     def solve(self, N, L=1, n_eq=1, **kwargs):
+        #print('Number of equations:', n_eq)
+
+        #print('setting resolution')
         self.set_resolution(N, L=L, n_eq=n_eq)
 
+        #print('calculating matrices')
         M = self.matrixM(**kwargs)
         S = self.vectorS(**kwargs)
 
+        #print('solving')
         return np.linalg.solve(M, S)
 
 class EigenValueSolver(SpectralSolver):
@@ -257,6 +289,8 @@ class EigenValueSolver(SpectralSolver):
     def solve(self, N, L=1, n_eq=1,
               sigma=None, n_eig=6,
               **kwargs):
+        #print('Number of equations:', n_eq)
+
         self.set_resolution(N, L=L, n_eq=n_eq)
 
         # Try and find *all* eigenvalues
@@ -329,6 +363,8 @@ class EigenValueSolver(SpectralSolver):
                    factor=2, drift_threshold=1e6, use_ordinal=False,
                    degeneracy=1, n_safe_levels=1,
                    **kwargs):
+        #print('Number of equations:', n_eq)
+
         N_high = int(factor**(n_safe_levels)*N)
         eval_hi, evec_hi = self.solve(N_high, L=L, n_eq=n_eq,
                                       sigma=sigma,
@@ -362,3 +398,273 @@ class EigenValueSolver(SpectralSolver):
         rad = np.max(np.abs(sigma - eval_low))
 
         return eval_hi, evec_hi, rad
+
+class SpectralSolver2D():
+    '''Spectral solvers for 2 independent variables
+
+    Matrix composition: A*B = [[a11*B, a12*B, ...], [a21*B, a22*B, ...], ...]
+
+    '''
+    def __init__(self,
+                 interval=[[0, 1], [0, 1]],
+                 symmetry=[None, None],
+                 basis=['Chebychev', 'Chebychev'],
+                 sparse_flag=True,
+                 use_PETSc=True):
+
+        # Create solver for x direction
+        self.spec_sol_x = \
+            SpectralSolver(interval[0],
+                           symmetry[0],
+                           basis[0],
+                           sparse_flag,
+                           use_PETSc)
+
+        self.spec_sol_z = \
+            SpectralSolver(interval[1],
+                           symmetry[1],
+                           basis[1],
+                           sparse_flag,
+                           use_PETSc)
+
+        self.sparse_flag = sparse_flag
+        self.use_PETSc = use_PETSc
+        if can_use_slepc == False:
+            self.use_PETSc = False
+
+        self.Nx = -1
+        self.Nz = -1
+
+    def set_resolution(self, Nx, Nz, Lx=1, Lz=1, n_eq=1):
+        #print('Number of equations:', n_eq)
+
+        self.spec_sol_x.set_resolution(Nx, L=Lx, n_eq=n_eq)
+        self.spec_sol_z.set_resolution(Nz, L=Lz, n_eq=n_eq)
+
+        #print('Constructing A')
+        #self.construct_a(n_eq=n_eq)
+
+    def mat_vect_combined(self, vec, A, B, f=None):
+        '''Matrix vector product linear operator'''
+        Nx = len(self.spec_sol_x.basis.collocation_points())
+        Nz = len(self.spec_sol_z.basis.collocation_points())
+
+        res = np.zeros(np.shape(vec), dtype=np.complex128)
+
+        for i in range(0, Nx):             # Block row number
+            if f is not None:
+                _B = np.matmul(np.diag(np.asarray(f(self.spec_sol_x.z[i], self.spec_sol_z.z))), B)
+            else:
+                _B = B
+
+            for j in range(0, Nx):             # Block column number
+                #print('Shapes: ', np.shape(A[i,j]*_B), np.shape(vec[j*Nz:(j+1)*Nz]), np.shape(vec), j*Nz, (j+1)*Nz)
+                res[j*Nz:(j+1)*Nz] += np.matmul(A[i,j]*_B, vec[j*Nz:(j+1)*Nz])
+
+        return res
+
+    def mat_vect_part_m(self, vec, kx=0, kz=0, f=None):
+        if kx + kz > 2:
+            print("ERROR: too large k in construct_matrix")
+
+        if kx == 0:
+            if kz == 0:
+                return self.mat_vect_combined(vec, self.spec_sol_x.A, self.spec_sol_z.A, f=f)
+            if kz == 1:
+                return self.mat_vect_combined(vec, self.spec_sol_x.A, self.spec_sol_z.dA, f=f)
+            if kz == 2:
+                return self.mat_vect_combined(vec, self.spec_sol_x.A, self.spec_sol_z.ddA, f=f)
+        if kx == 1:
+            if kz == 0:
+                return self.mat_vect_combined(vec, self.spec_sol_x.dA, self.spec_sol_z.A, f=f)
+            if kz == 1:
+                return self.mat_vect_combined(vec, self.spec_sol_x.dA, self.spec_sol_z.dA, f=f)
+            if kz == 2:
+                return self.mat_vect_combined(vec, self.spec_sol_x.dA, self.spec_sol_z.ddA, f=f)
+        if kx == 2:
+            if kz == 0:
+                return self.mat_vect_combined(vec, self.spec_sol_x.ddA, self.spec_sol_z.A, f=f)
+            if kz == 1:
+                return self.mat_vect_combined(vec, self.spec_sol_x.ddA, self.spec_sol_z.dA, f=f)
+            if kz == 2:
+                return self.mat_vect_combined(vec, self.spec_sol_x.ddA, self.spec_sol_z.ddA, f=f)
+
+    def construct_combined(self, A, B, f=None):
+        # Discretize a term f(x,z)*d^n W/dx^n
+        # Function f must have two parameters, x and z
+
+        Nx = len(self.spec_sol_x.basis.collocation_points())
+        Nz = len(self.spec_sol_z.basis.collocation_points())
+
+        indptr = np.asarray(range(0, Nx+1))*Nx
+
+        indices = []
+        data = []
+        for i in range(0, Nx):             # Block row number
+            indices.append(range(0, Nx))
+            if f is not None:
+                _B = np.matmul(np.diag(np.asarray(f(self.spec_sol_x.z[i], self.spec_sol_z.z))), B)
+            else:
+                _B = B
+
+            for j in range(0, Nx):         # Block column number
+                data.append(A[i,j]*_B)
+        data = np.asarray(data)
+
+        indices = np.asarray(indices).flatten()
+
+        ret = \
+            sparse.bsr_matrix((data, indices, indptr), shape=(Nx*Nz, Nx*Nz))
+
+        if self.sparse_flag == False:
+            ret = ret.todense()
+
+        return ret
+
+    def construct_a(self, n_eq=1, filename=None):
+        #print('Number of equations:', n_eq)
+
+        Nx = len(self.spec_sol_x.basis.collocation_points())
+        Nz = len(self.spec_sol_z.basis.collocation_points())
+
+        self.A = \
+            self.construct_combined(self.spec_sol_x.A, self.spec_sol_z.A)
+
+        # Make sure A is dense....
+        if self.sparse_flag == True:
+            A = self.A.todense()
+        else:
+            A = self.A
+
+        # Block-diagonal matrix with A's on the diagonal
+        indptr = np.asarray(range(0, n_eq + 1))
+        indices = np.asarray(range(0, n_eq))
+        data = []
+        for i in range(0, n_eq):
+            data.append(A)
+        data = np.asarray(data)
+
+        self.B = \
+          sparse.bsr_matrix((data, indices, indptr), shape=(n_eq*Nx*Nz, n_eq*Nx*Nz))
+
+        if self.sparse_flag == False:
+            self.B = self.B.todense()
+
+    def construct_rhs(self, f=None):
+        Nx = len(self.spec_sol_x.basis.collocation_points())
+        Nz = len(self.spec_sol_z.basis.collocation_points())
+
+        ret = np.zeros((Nx*Nz), dtype=np.complex64)
+
+        if f is not None:
+            for i in range(0, Nx):
+                x = self.spec_sol_x.z[i]
+                z = self.spec_sol_z.z
+
+                ret[Nz*i:Nz*i+Nz] = f(x, z)
+
+        return ret
+
+    def construct_m(self, f=None, kx=0, kz=0):
+        '''Distretize a term f(x, z)*d^n W/dx^n'''
+
+        if kx + kz > 2:
+            print("ERROR: too large k in construct_matrix")
+
+        if kx == 0:
+            if kz == 0:
+                return self.construct_combined(self.spec_sol_x.A, self.spec_sol_z.A, f=f)
+            if kz == 1:
+                return self.construct_combined(self.spec_sol_x.A, self.spec_sol_z.dA, f=f)
+            if kz == 2:
+                return self.construct_combined(self.spec_sol_x.A, self.spec_sol_z.ddA, f=f)
+        if kx == 1:
+            if kz == 0:
+                return self.construct_combined(self.spec_sol_x.dA, self.spec_sol_z.A, f=f)
+            if kz == 1:
+                return self.construct_combined(self.spec_sol_x.dA, self.spec_sol_z.dA, f=f)
+            if kz == 2:
+                return self.construct_combined(self.spec_sol_x.dA, self.spec_sol_z.ddA, f=f)
+        if kx == 2:
+            if kz == 0:
+                return self.construct_combined(self.spec_sol_x.ddA, self.spec_sol_z.A, f=f)
+            if kz == 1:
+                return self.construct_combined(self.spec_sol_x.ddA, self.spec_sol_z.dA, f=f)
+            if kz == 2:
+                return self.construct_combined(self.spec_sol_x.ddA, self.spec_sol_z.ddA, f=f)
+
+    def evaluate(self, x, z, sol, n_eq=1, kx=0, kz=0):
+        #print('Number of equations:', n_eq)
+
+        Nx = len(self.spec_sol_x.basis.collocation_points())
+        Nz = len(self.spec_sol_z.basis.collocation_points())
+
+        u = np.zeros((n_eq, Nx, len(z)), dtype=sol[0].dtype)
+
+        # First do inner sum for z
+        for n in range(0, n_eq):
+            for i in range(0, Nx):
+                w = self.spec_sol_z.evaluate(z, sol[n*Nx*Nz + i*Nz:n*Nx*Nz + i*Nz + Nz], n_eq=1, k=kz)
+                u[n, i, :] = w
+
+        ret = np.zeros((n_eq, len(x), len(z)), dtype=sol[0].dtype)
+
+        # Do outer sum for x
+        for n in range(0, n_eq):
+            for j in range(0, len(z)):
+                w = self.spec_sol_x.evaluate(x, u[n,:,j], n_eq=1, k=kx)
+                ret[n,:,j] = w
+
+        return ret
+
+    def transform(self, z, u, n_eq=1):
+        #print('Number of equations:', n_eq)
+
+        return u
+
+class BoundaryValueProblemSolver2D(SpectralSolver2D):
+    # uxx + uyy = S(x,y)
+    def vectorS(self, f=None):
+        return self.construct_rhs(f)
+
+    def mat_vect_m(self, vec):
+        # 2D Laplace operator
+        return self.mat_vect_part_m(vec, f=None, kx=2, kz=0) + \
+            self.mat_vect_part_m(vec, f=None, kx=0, kz=2)
+
+    def matrixM(self):
+        # 2D Laplace operator
+        return self.construct_m(f=None, kx=2, kz=0) + \
+            self.construct_m(f=None, kx=0, kz=2)
+
+    def solve(self, Nx, Nz, Lx=1, Lz=1, n_eq=1):
+        print('Setting resolution...')
+        #print('Number of equations:', n_eq)
+
+        self.set_resolution(Nx, Nz, Lx=Lx, Lz=Lz, n_eq=n_eq)
+
+        print('Filling matrices...')
+        M = self.matrixM()
+        S = self.vectorS()
+
+        print('Solving linear system...')
+        return np.linalg.solve(M, S)
+
+    def solve_operator(self, Nx, Nz, Lx=1, Lz=1, n_eq=1):
+        #print('Number of equations:', n_eq)
+
+        print('Setting resolution...')
+        self.set_resolution(Nx, Nz, Lx=Lx, Lz=Lz, n_eq=n_eq)
+
+        print('Calculating S')
+        S = self.vectorS()
+
+        print('Defining new operator')
+        Nx = len(self.spec_sol_x.basis.collocation_points())
+        Nz = len(self.spec_sol_z.basis.collocation_points())
+        op = LinearOperator((n_eq*Nx*Nz, n_eq*Nx*Nz), matvec=self.mat_vect_m)
+
+        ret, info = cgs(op, S)
+
+        return ret
+
